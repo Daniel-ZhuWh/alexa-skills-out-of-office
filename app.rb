ad7df49c7ae451d3ecb945a939c1646da6662af6
Daniel-ZhuWh <3292004808@qq.com>
2020-05-06 02:42:55 -0400

    xxx

--- app.rb
+++ app.rb
@@ -17,51 +17,66 @@ end
 # enable sessions for this project
 enable :sessions
 
-def update_status status, duration = nil
+def get_nutrients body
+  url = 'https://trackapi.nutritionix.com/v2/natural/nutrients'
+  res = HTTParty.post url, body: { query:body, timezone: "US/Eastern"}.to_json, headers: {'content-type' => 'application/json', 'x-app-id' => ENV['NUTRITIONIX_ID'], 'x-app-key' => ENV['NUTRITIONIX_KEY']}
 
-	# gets a corresponding message
-  message = get_message_for status, duration
-	# posts it to slack
-  post_to_slack status, message
-
-end
-
-def get_message_for status, duration
-
-	# Default response
-  message = "other/unknown"
-
-	# looks up a message based on the Status provided
-  if status == "HERE"
-    message = ENV['APP_USER'].to_s + " is in the office."
-  elsif status == "BACK_IN"
-    message = ENV['APP_USER'].to_s + " will be back in #{(duration/60).round} minutes"
-  elsif status == "BE_RIGHT_BACK"
-    message = ENV['APP_USER'].to_s + " will be right back"
-  elsif status == "GONE_HOME"
-    message = ENV['APP_USER'].to_s + " has left for the day. Check back tomorrow."
-  elsif status == "DO_NOT_DISTURB"
-    message = ENV['APP_USER'].to_s + " is busy. Please do not disturb."
+  total_fat = 0
+  total_protein = 0
+  total_cal = 0
+  res['foods'].each do |food|
+    total_fat += food['nf_total_fat']
+    total_protein += food['nf_protein']
+    total_cal += food['nf_calories']
   end
-
-	# return the appropriate message
-  message
-
+  msg = "You consumed #{total_cal} calories in total with #{total_protein} grams of protein and #{total_fat} grams of fat."
+  return msg
 end
-
-def post_to_slack status_update, message
-
-	# look up the Slack url from the env
-  slack_webhook = ENV['SLACK_WEBHOOK']
-
-	# create a formatted message
-  formatted_message = "*Status Changed for #{ENV['APP_USER'].to_s} to: #{status_update}*\n"
-  formatted_message += "#{message} "
-
-	# Post it to Slack
-  HTTParty.post slack_webhook, body: {text: formatted_message.to_s, username: "OutOfOfficeBot", channel: "back" }.to_json, headers: {'content-type' => 'application/json'}
-
-end
+# def update_status status, duration = nil
+#
+# 	# gets a corresponding message
+#   message = get_message_for status, duration
+# 	# posts it to slack
+#   post_to_slack status, message
+#
+# end
+#
+# def get_message_for status, duration
+#
+# 	# Default response
+#   message = "other/unknown"
+#
+# 	# looks up a message based on the Status provided
+#   if status == "HERE"
+#     message = ENV['APP_USER'].to_s + " is in the office."
+#   elsif status == "BACK_IN"
+#     message = ENV['APP_USER'].to_s + " will be back in #{(duration/60).round} minutes"
+#   elsif status == "BE_RIGHT_BACK"
+#     message = ENV['APP_USER'].to_s + " will be right back"
+#   elsif status == "GONE_HOME"
+#     message = ENV['APP_USER'].to_s + " has left for the day. Check back tomorrow."
+#   elsif status == "DO_NOT_DISTURB"
+#     message = ENV['APP_USER'].to_s + " is busy. Please do not disturb."
+#   end
+#
+# 	# return the appropriate message
+#   message
+#
+# end
+#
+# def post_to_slack status_update, message
+#
+# 	# look up the Slack url from the env
+#   slack_webhook = ENV['SLACK_WEBHOOK']
+#
+# 	# create a formatted message
+#   formatted_message = "*Status Changed for #{ENV['APP_USER'].to_s} to: #{status_update}*\n"
+#   formatted_message += "#{message} "
+#
+# 	# Post it to Slack
+#   HTTParty.post slack_webhook, body: {text: formatted_message.to_s, username: "OutOfOfficeBot", channel: "back" }.to_json, headers: {'content-type' => 'application/json'}
+#
+# end
 # ----------------------------------------------------------------------
 #     How you handle your Alexa
 # ----------------------------------------------------------------------
@@ -73,59 +88,59 @@ class CustomHandler < AlexaSkillsRuby::Handler
     logger.info 'Lauch request processed'
   end
 
-  on_intent("GetZodiacHoroscopeIntent") do
-    slots = request.intent.slots
-    response.set_output_speech_text("Horoscope Text")
-    #response.set_output_speech_ssml("<speak><p>Horoscope Text</p><p>More Horoscope text</p></speak>")
-    response.set_reprompt_speech_text("Reprompt Horoscope Text")
-    #response.set_reprompt_speech_ssml("<speak>Reprompt Horoscope Text</speak>")
-    response.set_simple_card("title", "content")
-    logger.info 'GetZodiacHoroscopeIntent processed'
-  end
-
-  on_intent("HERE") do
-    # add a response to Alexa
-    response.set_output_speech_text("I've updated your status to Here ")
-    # create a card response in the alexa app
-    response.set_simple_card("Out of Office App", "Status is in the office.")
-    # log the output if needed
-    logger.info 'Here processed'
-    # send a message to slack
-    update_status "HERE"
-  end
-
-  on_intent("BE_RIGHT_BACK") do
-    # add a response to Alexa
-    response.set_output_speech_text("I've updated your status to BE_RIGHT_BACK ")
-    # create a card response in the alexa app
-    response.set_simple_card("Out of Office App", "Status will be right back.")
-    # log the output if needed
-    logger.info 'BE_RIGHT_BACK processed'
-    # send a message to slack
-    update_status "BE_RIGHT_BACK"
-  end
-
-  on_intent("GONE_HOME") do
-    # add a response to Alexa
-    response.set_output_speech_text("I've updated your status to GONE_HOME ")
-    # create a card response in the alexa app
-    response.set_simple_card("Out of Office App", "Status has gone home.")
-    # log the output if needed
-    logger.info 'GONE_HOME processed'
-    # send a message to slack
-    update_status "GONE_HOME"
-  end
-
-  on_intent("DO_NOT_DISTURB") do
-    # add a response to Alexa
-    response.set_output_speech_text("I've updated your status to DO_NOT_DISTURB ")
-    # create a card response in the alexa app
-    response.set_simple_card("Out of Office App", "Status is DO_NOT_DISTURB.")
-    # log the output if needed
-    logger.info 'DO_NOT_DISTURB processed'
-    # send a message to slack
-    update_status "DO_NOT_DISTURB"
-  end
+  # on_intent("GetZodiacHoroscopeIntent") do
+  #   slots = request.intent.slots
+  #   response.set_output_speech_text("Horoscope Text")
+  #   #response.set_output_speech_ssml("<speak><p>Horoscope Text</p><p>More Horoscope text</p></speak>")
+  #   response.set_reprompt_speech_text("Reprompt Horoscope Text")
+  #   #response.set_reprompt_speech_ssml("<speak>Reprompt Horoscope Text</speak>")
+  #   response.set_simple_card("title", "content")
+  #   logger.info 'GetZodiacHoroscopeIntent processed'
+  # end
+  #
+  # on_intent("HERE") do
+  #   # add a response to Alexa
+  #   response.set_output_speech_text("I've updated your status to Here ")
+  #   # create a card response in the alexa app
+  #   response.set_simple_card("Out of Office App", "Status is in the office.")
+  #   # log the output if needed
+  #   logger.info 'Here processed'
+  #   # send a message to slack
+  #   update_status "HERE"
+  # end
+  #
+  # on_intent("BE_RIGHT_BACK") do
+  #   # add a response to Alexa
+  #   response.set_output_speech_text("I've updated your status to BE_RIGHT_BACK ")
+  #   # create a card response in the alexa app
+  #   response.set_simple_card("Out of Office App", "Status will be right back.")
+  #   # log the output if needed
+  #   logger.info 'BE_RIGHT_BACK processed'
+  #   # send a message to slack
+  #   update_status "BE_RIGHT_BACK"
+  # end
+  #
+  # on_intent("GONE_HOME") do
+  #   # add a response to Alexa
+  #   response.set_output_speech_text("I've updated your status to GONE_HOME ")
+  #   # create a card response in the alexa app
+  #   response.set_simple_card("Out of Office App", "Status has gone home.")
+  #   # log the output if needed
+  #   logger.info 'GONE_HOME processed'
+  #   # send a message to slack
+  #   update_status "GONE_HOME"
+  # end
+  #
+  # on_intent("DO_NOT_DISTURB") do
+  #   # add a response to Alexa
+  #   response.set_output_speech_text("I've updated your status to DO_NOT_DISTURB ")
+  #   # create a card response in the alexa app
+  #   response.set_simple_card("Out of Office App", "Status is DO_NOT_DISTURB.")
+  #   # log the output if needed
+  #   logger.info 'DO_NOT_DISTURB processed'
+  #   # send a message to slack
+  #   update_status "DO_NOT_DISTURB"
+  # end
 
   on_intent("REQUEST_TO_LOG_MEAL") do
     # add a response to Alexa
@@ -141,9 +156,11 @@ class CustomHandler < AlexaSkillsRuby::Handler
   on_intent("LOG_MEAL") do
     food_log = request.intent.slots["food_log"]
     # add a response to Alexa
-    response.set_output_speech_text("I've logged #{food_log}")
+    res = get_nutrients food_log
+
+    response.set_output_speech_text("#{res}")
     # create a card response in the alexa app
-    response.set_simple_card("Out of Office App", "Status is DO_NOT_DISTURB.")
+    response.set_simple_card("Diet Bot App", "#{res}")
     # log the output if needed
     logger.info 'LOG_MEAL processed'
     # send a message to slack
@@ -163,42 +180,42 @@ class CustomHandler < AlexaSkillsRuby::Handler
     logger.info 'HelpIntent processed'
   end
 
-  on_intent("BACK_IN") do
-
-		# Access the slots
-    slots = request.intent.slots
-    puts slots.to_s
-
-		# Duration is returned in a particular format
-		# Called ISO8601. Translate this into seconds
-    duration = ISO8601::Duration.new( request.intent.slots["duration"] ).to_seconds
-
-		# This will downsample the duration from a default seconds
-		# To...
-    if duration > 60 * 60 * 24
-      days = duration/(60 * 60 * 24).round
-      response.set_output_speech_text("I've set you away for #{ days } days")
-    elsif duration > 60 * 60
-      hours = duration/(60 * 60 ).round
-      response.set_output_speech_text("I've set you away for #{ hours } hours")
-    else
-      mins = duration/(60).round
-      response.set_output_speech_text("I've set you away for #{ mins } minutes")
-    end
-    logger.info 'BackIn processed'
-    update_status "BACK_IN", duration
-  end
-
-  on_intent("TEST") do
-    response.set_output_speech_ssml("<speak>
-    Welcome to Car-Fu.
-    <audio src='soundbank://soundlibrary/transportation/amzn_sfx_car_accelerate_01' />
-    You can order a ride, or request a fare estimate.
-    Which will it be?
-    </speak>")
-
-    logger.info 'TEST processed'
-  end
+  # on_intent("BACK_IN") do
+  #
+	# 	# Access the slots
+  #   slots = request.intent.slots
+  #   puts slots.to_s
+  #
+	# 	# Duration is returned in a particular format
+	# 	# Called ISO8601. Translate this into seconds
+  #   duration = ISO8601::Duration.new( request.intent.slots["duration"] ).to_seconds
+  #
+	# 	# This will downsample the duration from a default seconds
+	# 	# To...
+  #   if duration > 60 * 60 * 24
+  #     days = duration/(60 * 60 * 24).round
+  #     response.set_output_speech_text("I've set you away for #{ days } days")
+  #   elsif duration > 60 * 60
+  #     hours = duration/(60 * 60 ).round
+  #     response.set_output_speech_text("I've set you away for #{ hours } hours")
+  #   else
+  #     mins = duration/(60).round
+  #     response.set_output_speech_text("I've set you away for #{ mins } minutes")
+  #   end
+  #   logger.info 'BackIn processed'
+  #   update_status "BACK_IN", duration
+  # end
+  #
+  # on_intent("TEST") do
+  #   response.set_output_speech_ssml("<speak>
+  #   Welcome to Car-Fu.
+  #   <audio src='soundbank://soundlibrary/transportation/amzn_sfx_car_accelerate_01' />
+  #   You can order a ride, or request a fare estimate.
+  #   Which will it be?
+  #   </speak>")
+  #
+  #   logger.info 'TEST processed'
+  # end
 end
 
 # ----------------------------------------------------------------------
